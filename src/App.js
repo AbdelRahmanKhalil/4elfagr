318dfda1807e928c6531ba7cb3870f863ccba938
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-09-24 05:46:49 +0200

    starter site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar"; 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import HeroSection from "./components/HeroSection";
+import Container from "@mui/material/Container";
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#030304',
+    },
+    secondary: {
+      main: '#ff0046',
+    },
+  },
+});
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar>
+          <HeroSection title="Changing the arabic music scene forever." video="/video-3.mp4"/>
+          <HeroSection video="/video-4.mp4"/>
+          
+          <iframe src="https://widget.anghami.com/artist/6083539/?theme=fulllight&layout=list&lang=en" scrolling="no" frameborder="0" width="100%" height={300} ></iframe>
+          {/* <iframe src="https://widget.anghami.com/artist/6083539/?theme=fulllight&layout=list&lang=en" scrolling="no" frameborder="0" width={600} height={450} ></iframe> */}
+          <iframe src="https://open.spotify.com/embed/artist/5OohOwlYO1awBrbL71R4fv" width="100%" height="300" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+          {/* <iframe frameborder="0" height="450" width="100%"  src="https://embed.music.apple.com/us/artist/4-el-fagr/1502946546"></iframe> */}
+        <Switch>
+          {/* <Route path='/UI-Design-Trial/' exact component= {Home} />
+          <Route path='/UI-Design-Trial/ProgressPage' exact component= {ProgressPage} /> */}
+        </Switch>
+        </Navbar>
+      </Router>
+      </ThemeProvider>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/HeroSection.js
@@ -0,0 +1,33 @@
+import '../App.css';
+import './HeroSection.css';
+import React, { Component } from 'react'
+
+
+export default class HeroSection extends Component {
+    render() {
+        
+        let background;
+        if (this.props.image != null) {
+            const img_path = process.env.PUBLIC_URL + this.props.image
+            console.log(img_path)
+            background = <img src={img_path} alt=""/>
+        } else if (this.props.video != null) {
+            background = <video src={this.props.video} autoPlay loop muted />
+        } else {
+            background = <img src="/img-0.jpg" alt=""/>
+        }
+        console.log(background)
+        return (
+            <div className='hero-container'>
+                {background}
+                
+
+                <h1 className="entry-title">{this.props.title}</h1>
+                
+                <p>{this.props.paragraph}</p>
+                
+            </div>
+        )
+    }
+}
+
