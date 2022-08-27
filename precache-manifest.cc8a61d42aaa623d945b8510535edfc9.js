7f97c2c55d283f6ef63df109fb49e7df06d00c77
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2022-08-27 03:10:19 +0200

    Updates

---
+++ precache-manifest.cc8a61d42aaa623d945b8510535edfc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d360499faec9700af882",
+    "url": "/4elfagr/static/css/main.f1beeee6.chunk.css"
+  },
+  {
+    "revision": "d360499faec9700af882",
+    "url": "/4elfagr/static/js/main.d360499f.chunk.js"
+  },
+  {
+    "revision": "4f82f7d665ba133890a0",
+    "url": "/4elfagr/static/js/1.4f82f7d6.chunk.js"
+  },
+  {
+    "revision": "f4b4329f1554c567db81",
+    "url": "/4elfagr/static/js/2.f4b4329f.chunk.js"
+  },
+  {
+    "revision": "c1ff212ab9848f77a3cf",
+    "url": "/4elfagr/static/js/runtime~main.c1ff212a.js"
+  },
+  {
+    "revision": "7fdce3d4ef2db27e0476807efa4ccce3",
+    "url": "/4elfagr/index.html"
+  }
+];
